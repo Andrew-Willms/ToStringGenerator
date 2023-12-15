89b1003cb8114cd1f4d2c7b4fd0943dddc64b44f
Andrew-Willms <andrew.e.willms@gmail.com>
2023-12-15 00:26:10 -0500

    Created GetClassNesting extension method

---
+++ ToStringGenerator/CodeAnalysisUtilities/ClassNesting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+
+namespace CodeAnalysisUtilities; 
+
+
+
+public static class ClassNesting {
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="classSymbol"></param>
+	/// <returns></returns>
+	public static List<INamedTypeSymbol> GetClassNesting(this INamedTypeSymbol classSymbol) {
+
+		if (classSymbol.ContainingType is null) {
+			return new() { classSymbol };
+		}
+
+		List<INamedTypeSymbol> classNesting = GetClassNesting(classSymbol.ContainingType);
+
+		classNesting.Add(classSymbol);
+
+		return classNesting;
+	}
+
+}
