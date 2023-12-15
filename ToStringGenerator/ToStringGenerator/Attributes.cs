3bd60491b13bc13c60363b6da7309a4c795fff23
Andrew-Willms <andrew.e.willms@gmail.com>
2023-12-14 22:38:47 -0500

    Added attribute generators.

---
+++ ToStringGenerator/ToStringGenerator/Attributes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ToStringGenerator;
+
+
+
+/// <summary>
+/// 
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
+public class GeneratorToStringAttribute : Attribute;
+
+
+
+/// <summary>
+/// 
+/// </summary>
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+public class ExcludeFromToStringAttribute : Attribute;
+
+
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="T"></typeparam>
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+public class ToStringFormatAttribute<T> : Attribute {
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="formatter"></param>
+	public ToStringFormatAttribute(Func<T, string> formatter) {
+
+	}
+
+}
