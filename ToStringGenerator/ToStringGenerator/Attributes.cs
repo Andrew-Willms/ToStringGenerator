be9ba010688ab012066795d6a579fb3ba623695f
Andrew-Willms <andrew.e.willms@gmail.com>
2023-12-15 00:33:11 -0500

    began working on source generator

--- ToStringGenerator/ToStringGenerator/Attributes.cs
+++ ToStringGenerator/ToStringGenerator/Attributes.cs
@@ -8,7 +8,12 @@ namespace ToStringGenerator;
 /// 
 /// </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
-public class GeneratorToStringAttribute : Attribute;
+public class GeneratorToStringAttribute : Attribute {
+
+	// todo this doesn't feel quite right but I will leave it for now
+	internal static readonly string Name = $"global::{typeof(GeneratorToStringAttribute).FullName!}";
+
+}
 
 
 
