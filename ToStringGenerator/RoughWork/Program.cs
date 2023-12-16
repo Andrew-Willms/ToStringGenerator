ae0cd505d8b75ed57e53508ee92e08a824789986
Andrew-Willms <andrew.e.willms@gmail.com>
2023-12-16 16:09:34 -0500

    type property testing

--- ToStringGenerator/RoughWork/Program.cs
+++ ToStringGenerator/RoughWork/Program.cs
@@ -8,8 +8,25 @@ public class Program {
 
 	public static void Main(params string[] args) {
 
-		Console.WriteLine("Hello World");
+		Console.WriteLine(typeof(GenericAttribute<int>).Namespace);
+		Console.WriteLine(typeof(GenericAttribute<int>).Name);
+		Console.WriteLine(typeof(GenericAttribute<>).Name);
+		Console.WriteLine(typeof(GenericAttribute<>).ContainsGenericParameters);
+		Console.WriteLine(typeof(GenericAttribute<int>).ContainsGenericParameters);
+		Console.WriteLine(typeof(GenericAttribute<>).IsConstructedGenericType);
+		Console.WriteLine(typeof(GenericAttribute<int>).IsConstructedGenericType);
+		Console.WriteLine(typeof(GenericAttribute<>).IsGenericType);
+		Console.WriteLine(typeof(GenericAttribute<int>).IsGenericType);
+		Console.WriteLine(typeof(GenericAttribute<>).IsGenericTypeDefinition);
+		Console.WriteLine(typeof(GenericAttribute<int>).IsGenericTypeDefinition);
 
 	}
+
+}
+
+
+
+
+public class GenericAttribute<T> : Attribute {
 
 }
