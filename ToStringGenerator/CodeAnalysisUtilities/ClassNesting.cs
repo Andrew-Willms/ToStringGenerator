9505ba894bf6f1d584577b6e6de7dea909b52758
Andrew-Willms <andrew.e.willms@gmail.com>
2023-12-16 00:02:50 -0500

    added function interface for more ClassNesting extension methods

--- ToStringGenerator/CodeAnalysisUtilities/ClassNesting.cs
+++ ToStringGenerator/CodeAnalysisUtilities/ClassNesting.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.CodeAnalysis;
 
 namespace CodeAnalysisUtilities; 
@@ -8,8 +8,10 @@ namespace CodeAnalysisUtilities;
 
 public static class ClassNesting {
 
+	// todo use cref to link to the appropriate data types
 	/// <summary>
-	/// 
+	/// Returns a List of INamedTypeSymbols representing the hierarchy of nesting.
+	/// The first element in the list is the outermost class, the last element is the class this extension method is called on.
 	/// </summary>
 	/// <param name="classSymbol"></param>
 	/// <returns></returns>
@@ -24,6 +26,26 @@ public static class ClassNesting {
 		classNesting.Add(classSymbol);
 
 		return classNesting;
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="nestingHierarchy"></param>
+	/// <returns></returns>
+	/// <exception cref="NotImplementedException"></exception>
+	public static string GenerateNestedClassOpening(IEnumerable<INamedTypeSymbol> nestingHierarchy) {
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="nestingHierarchy"></param>
+	/// <returns></returns>
+	/// <exception cref="NotImplementedException"></exception>
+	public static string GenerateNestedClassClosing(IEnumerable<INamedTypeSymbol> nestingHierarchy) {
+		throw new NotImplementedException();
 	}
 
 }
