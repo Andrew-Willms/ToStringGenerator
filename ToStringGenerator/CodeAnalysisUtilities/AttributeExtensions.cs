2e76a82267f91010381d229807f5fd1e9e20558d
Andrew-Willms <andrew.e.willms@gmail.com>
2023-12-16 00:02:08 -0500

    Added AttributeExtensions

---
+++ ToStringGenerator/CodeAnalysisUtilities/AttributeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace CodeAnalysisUtilities; 
+
+
+
+public static class AttributeExtensions {
+
+	public static bool HasAttribute<TAttribute>(this ISymbol symbol) {
+
+		return symbol
+			.GetAttributes()
+			.Any(attribute => symbol.Equals(attribute?.AttributeClass));
+	}
+
+}
