c76ef6c5de6cc021e8f7d76111b1745e9ec9cfa5
Andrew-Willms <andrew.e.willms@gmail.com>
2023-12-17 00:19:21 -0500

    rough work string testing

--- ToStringGenerator/RoughWork/Program.cs
+++ ToStringGenerator/RoughWork/Program.cs
@@ -8,17 +8,22 @@ public class Program {
 
 	public static void Main(params string[] args) {
 
-		Console.WriteLine(typeof(GenericAttribute<int>).Namespace);
-		Console.WriteLine(typeof(GenericAttribute<int>).Name);
-		Console.WriteLine(typeof(GenericAttribute<>).Name);
-		Console.WriteLine(typeof(GenericAttribute<>).ContainsGenericParameters);
-		Console.WriteLine(typeof(GenericAttribute<int>).ContainsGenericParameters);
-		Console.WriteLine(typeof(GenericAttribute<>).IsConstructedGenericType);
-		Console.WriteLine(typeof(GenericAttribute<int>).IsConstructedGenericType);
-		Console.WriteLine(typeof(GenericAttribute<>).IsGenericType);
-		Console.WriteLine(typeof(GenericAttribute<int>).IsGenericType);
-		Console.WriteLine(typeof(GenericAttribute<>).IsGenericTypeDefinition);
-		Console.WriteLine(typeof(GenericAttribute<int>).IsGenericTypeDefinition);
+		string text = "2";
+		Console.WriteLine(text.Substring(1));
+		Console.WriteLine(text[1..]);
+		Console.WriteLine(text);
+
+		//Console.WriteLine(typeof(GenericAttribute<int>).Namespace);
+		//Console.WriteLine(typeof(GenericAttribute<int>).Name);
+		//Console.WriteLine(typeof(GenericAttribute<>).Name);
+		//Console.WriteLine(typeof(GenericAttribute<>).ContainsGenericParameters);
+		//Console.WriteLine(typeof(GenericAttribute<int>).ContainsGenericParameters);
+		//Console.WriteLine(typeof(GenericAttribute<>).IsConstructedGenericType);
+		//Console.WriteLine(typeof(GenericAttribute<int>).IsConstructedGenericType);
+		//Console.WriteLine(typeof(GenericAttribute<>).IsGenericType);
+		//Console.WriteLine(typeof(GenericAttribute<int>).IsGenericType);
+		//Console.WriteLine(typeof(GenericAttribute<>).IsGenericTypeDefinition);
+		//Console.WriteLine(typeof(GenericAttribute<int>).IsGenericTypeDefinition);
 
 	}
 
