5c8d2672825b74881828c18e92c04bdea470fc1e
Andrew-Willms <andrew.e.willms@gmail.com>
2023-12-17 00:20:01 -0500

    created linq extensions project

---
+++ ToStringGenerator/LinqUtilities/LinqExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqUtilities; 
+
+
+
+// todo from elsewhere and not used here, find a good home for these
+public static class LinqExtensions {
+
+	public static IEnumerable<T> Exclude<T>(this IEnumerable<T> enumerable, T excludedValue) {
+		return enumerable.Where(x => !Equals(x, excludedValue));
+	}
+
+	public static IEnumerable<T> Exclude<T>(this IEnumerable<T> enumerable, params T[] excludedValues) {
+		return enumerable.Where(x => !excludedValues.Contains(x));
+	}
+
+	public static IEnumerable<T> Exclude<T>(this IEnumerable<T> enumerable, Func<T, bool> predicate) {
+		return enumerable.Where(x => !predicate(x));
+	}
+
+}
---
+++ ToStringGenerator/LinqUtilities/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LinqUtilities;
+
+
+
+public static class StringExtensions {
+
+	public static string Join(this IEnumerable<string> enumerable) {
+		return string.Join(string.Empty, enumerable);
+	}
+
+	public static string Join(this IEnumerable<string> enumerable, string separator) {
+		return string.Join(separator, enumerable);
+	}
+
+	public static string LowerFirstLetter(this string text) {
+
+		return string.IsNullOrWhiteSpace(text)
+			? text
+			: char.ToLower(text[0]) + text.Substring(1);
+	}
+
+}
