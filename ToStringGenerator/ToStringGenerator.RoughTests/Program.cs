1f447db7227e247472fb08f8ea850a18a79cbdd0
Andrew-Willms <andrew.e.willms@gmail.com>
2023-12-18 23:07:01 -0500

    added RoughTests project

---
+++ ToStringGenerator/ToStringGenerator.RoughTests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ToStringGenerator.RoughTests;
+
+
+
+internal class Program {
+	
+	public static void Main(string[] args) {
+		Console.WriteLine("Hello, World!");
+	}
+
+}
