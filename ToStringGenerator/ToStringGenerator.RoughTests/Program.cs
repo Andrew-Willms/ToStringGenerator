9f0e723e78a350a7534dd2e290661e6a05f7d566
Andrew-Willms <andrew.e.willms@gmail.com>
2023-12-20 00:52:54 -0500

    added second property to test class

--- ToStringGenerator/ToStringGenerator.RoughTests/Program.cs
+++ ToStringGenerator/ToStringGenerator.RoughTests/Program.cs
@@ -18,9 +18,11 @@ internal class Program {
 
 
 
-[GenerateToString]
+[GenerateToString(PropertyPolicy = AccessModifier.All)]
 public class TestClass {
 
-	public string TestString { get; set; } = "Hello World";
+	public string TestString1 { get; set; } = "Hello World";
+
+	public string TestString2 { get; set; } = "Hello Other World";
 
 }
